fb8d23bd36f8d37a3739df72f13836481106d62e
Mahid1856502 <mahidshamshad@gmail.com>
2023-07-25 18:48:31 +0500

    first commit

---
+++ src/components/ContactUs.jsx
@@ -0,0 +1,163 @@
+import React, { useState } from "react";
+import { Button, Card, Col, Container, Form, Row } from "react-bootstrap";
+import { IMAGES } from "../assets/images";
+
+export default function ContactUs({ contact }) {
+  const [value, setValue] = useState({
+    name: "",
+    email: "",
+    subject: "",
+    message: "",
+  });
+  const [validated, setValidated] = useState(false);
+
+  const handleChange = (e) => {
+    setValue((preState) => ({
+      ...preState,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const handleSubmit = (event) => {
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+    event.preventDefault();
+    setValidated(true);
+  };
+
+  return (
+    <Container fluid className="px-4 px-sm-5 pb-5" ref={contact}>
+      <Row className="my-5 py-5">
+        <Col md={4}>
+          <h4 className="text-primary">STAY IN TOUCH</h4>
+          <h1 className="mb-1 fw-bold">Contact us</h1>
+          <img src={IMAGES.BARS} alt="" />
+          <Row>
+            <Col xs="12" className="mt-4">
+              <img
+                src={IMAGES.PHONE}
+                alt=""
+                width="35px"
+                className="me-3 me-sm-2 me-lg-5"
+              />
+              1-541-754-3010
+            </Col>
+            <Col xs="12" className="mt-4 pe-0">
+              <img
+                src={IMAGES.ENVELOPE}
+                alt=""
+                width="30px"
+                className="ms-1 me-3 me-sm-2 me-lg-5"
+              />
+              support@jointreaty.com
+            </Col>
+            <Col xs="12" className="my-4 pe-0">
+              <img
+                src={IMAGES.POINTER}
+                alt=""
+                width="30px"
+                className="ms-1 me-3 me-sm-2 me-lg-5"
+              />
+              3782 Shady Pines Drive USA
+            </Col>
+            <Col
+              xs="12"
+              className="my-4 d-flex d-md-block justify-content-center"
+            >
+              <div>
+                <img
+                  className="me-2 me-lg-4 mt-3"
+                  src={IMAGES.FB}
+                  alt=""
+                  style={{ width: "3rem", cursor: "pointer" }}
+                />
+                <img
+                  className="me-2 me-lg-4 mt-3"
+                  src={IMAGES.TWITTER}
+                  alt=""
+                  style={{ width: "2.9rem", cursor: "pointer" }}
+                />
+                <img
+                  className="me-2 me-lg-4 mt-3"
+                  src={IMAGES.INSTA}
+                  alt=""
+                  style={{ width: "2.9rem", cursor: "pointer" }}
+                />
+                <img
+                  className="me-2 me-lg-4 mt-3"
+                  src={IMAGES.YOUTUBE}
+                  alt=""
+                  style={{ width: "2.9rem", cursor: "pointer" }}
+                />
+              </div>
+            </Col>
+          </Row>
+        </Col>
+        <Col md={8}>
+          <Card className="p-4 p-md-5 border-0 rounded-0 shadow">
+            <Form noValidate validated={validated} onSubmit={handleSubmit}>
+              <Row>
+                <Col md={6}>
+                  <Form.Group>
+                    <Form.Control
+                      required
+                      className="rounded-0 p-3 mb-3"
+                      name="name"
+                      value={value.name}
+                      onChange={handleChange}
+                      type="text"
+                      placeholder="NAME *"
+                    />
+                  </Form.Group>
+                  <Form.Group>
+                    <Form.Control
+                      required
+                      className="rounded-0 p-3 mb-3"
+                      name="email"
+                      value={value.email}
+                      onChange={handleChange}
+                      type="email"
+                      placeholder="EMAIL *"
+                    />
+                  </Form.Group>
+                  <Form.Group>
+                    <Form.Control
+                      className="rounded-0 p-3 mb-3"
+                      name="subject"
+                      value={value.subject}
+                      onChange={handleChange}
+                      type="text"
+                      placeholder="SUBJECT *"
+                    />
+                  </Form.Group>
+                </Col>
+                <Col md={6}>
+                  <Form.Group>
+                    <Form.Control
+                      required
+                      className="rounded-0 p-3 mb-3"
+                      name="message"
+                      value={value.message}
+                      onChange={handleChange}
+                      as="textarea"
+                      rows={7}
+                      placeholder="MESSAGE *"
+                    />
+                  </Form.Group>
+                </Col>
+                <Col xs={12} className="d-flex justify-content-end">
+                  <Button className="mt-3" type="submit">
+                    Submit form
+                  </Button>
+                </Col>
+              </Row>
+            </Form>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
+  );
+}
---
+++ src/components/Header.jsx
@@ -0,0 +1,82 @@
+import React, { useState } from "react";
+import { Button, Container, Nav, Navbar } from "react-bootstrap";
+import { IMAGES } from "../assets/images";
+import { RxHamburgerMenu } from "react-icons/rx";
+
+export default function Header(props) {
+  const [active, setActive] = useState("Home");
+
+  const { home, about, services, contact } = props;
+
+  const handleClick = (section) => {
+    window.scrollTo({
+      top: section.current.offsetTop - 90,
+      behaviour: "smooth",
+    });
+  };
+
+  const navLinks = [
+    { label: "Home", ref: home },
+    { label: "About us", ref: about },
+    { label: "Services", ref: services },
+    { label: "Contact us", ref: contact },
+  ];
+
+  return (
+    <Navbar
+      bg="white"
+      expand="lg"
+      className="shadow-sm header-wrapper sticky-top py-sm-3 py-2"
+    >
+      <Container fluid className="px-md-5 px-4">
+        <Navbar.Brand className="pointer">
+          <div
+            className="d-flex align-items-center"
+            onClick={() => {
+              handleClick(home);
+              setActive("Home");
+            }}
+          >
+            <img src={IMAGES.LOGO} alt="logo" className="main-logo-icon" />
+            <h2 className="ms-2 main-logo">treaty</h2>
+          </div>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav">
+          <RxHamburgerMenu size={24} className="text-black" />
+        </Navbar.Toggle>
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto d-flex align-items-center ">
+            {navLinks.map((item) => (
+              <div>
+                <Nav.Link
+                  className={`ms-3 text-primary fw-regular fs-md d-flex justify-content-center nav-parent ${
+                    active === item.label ? "active" : ""
+                  }`}
+                  onClick={() => {
+                    handleClick(item.ref);
+                    setActive(item.label);
+                  }}
+                >
+                  {item.label}
+                  <div
+                    className={`${
+                      active === item.label
+                        ? "nav-border d-lg-block d-none"
+                        : ""
+                    }`}
+                  />
+                </Nav.Link>
+              </div>
+            ))}
+            <Button
+              className="ms-3 rounded-pill fs-sm my-md-0 my-2"
+              variant="outline-primary"
+            >
+              Login/Register
+            </Button>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
